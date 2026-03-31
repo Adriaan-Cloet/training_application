7614d41988f3ee20f6cecdc3b80f42e4f776e6ea
Adriaan-Cloet <r0928701@ucll.be>
2026-03-31 16:43:46 +0200

    made training list component

---
+++ triathlon-app/src/app/components/training-list/training-list.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TrainingService } from '../../services/training.service';
+import { Training } from '../../models/training.model';
+
+@Component({
+  selector: 'app-training-list',
+  imports: [CommonModule],
+  templateUrl: './training-list.html',
+  styleUrl: './training-list.css',
+})
+export class TrainingList implements OnInit {
+  trainings: Training[] = [];
+
+  constructor(private trainingService: TrainingService) {}
+
+  ngOnInit() {
+    this.load();
+  }
+
+  load() {
+    this.trainings = this.trainingService.getAll().sort((a, b) => {
+      return new Date(b.date).getTime() - new Date(a.date).getTime();
+    });
+  }
+
+  delete(id: string) {
+    this.trainingService.delete(id);
+    this.load();
+  }
+}

ca07cccad7cfb6ab1aea0614f04764057a8cf5d2
Adriaan-Cloet <r0928701@ucll.be>
2026-04-18 17:51:40 +0200

    US-006 app layout met header en responsive grid

--- triathlon-app/src/app/app.ts
+++ triathlon-app/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { TrainingFormComponent } from './components/training-form/training-form';
 import { TrainingListComponent } from './components/training-list/training-list';
+import { HeaderComponent } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TrainingFormComponent, TrainingListComponent],
+  imports: [TrainingFormComponent, TrainingListComponent, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
